0bb5222474b2d4c9d8400cdc2703d980e022b112
freefireman <beluh.c.s@gmail.com>
2021-04-07 18:32:24 +0300

    i wrote your code little bit))

--- router-hw/src/components/Posts/Posts.js
+++ router-hw/src/components/Posts/Posts.js
@@ -1,43 +1,28 @@
 import React from 'react'
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link,
-    Redirect,
-    useParams,
-} from "react-router-dom";
+import {Route, NavLink} from "react-router-dom";
 import PostDetail from './PostDetail';
 
 export default function Posts() {
+
     const [posts, setPosts] = React.useState([])
 
-const fetchData = async () => {
-    const resp = await fetch('https://jsonplaceholder.typicode.com/posts')
-    const json = await resp.json()
-    setPosts(json)
-}
+    const fetchData = async () => {
+        const resp = await fetch('https://jsonplaceholder.typicode.com/posts')
+        const json = await resp.json()
+        setPosts(json)
+    }
 
-
-    React.useEffect(()=>{
+    React.useEffect(() => {
         fetchData()
     }, [])
 
     return (
         <div>
-        <Switch>
-
-          <Route path="/posts/:id">
-            <PostDetail/>
-          </Route>
-
-          <Route >
-            <Redirect to="/posts"/>
-          </Route>
-        
-        </Switch> 
+            <Route path="/posts/:id" component={PostDetail}/>
             <ul>
-                {posts.map(el => <Link to={`/posts/${el.id}`}><li>{el.title} - {el.id}</li></Link>)}          
+                {posts.map(el => <NavLink to={`/posts/${el.id}`}>
+                    <li>{el.title} - {el.id}</li>
+                </NavLink>)}
             </ul>
         </div>
     )

ed1ba525c9c410ee74f43f403a63e95f1b52466d
freefireman <beluh.c.s@gmail.com>
2021-04-07 16:08:02 +0300

    i wrote your code little bit))

--- router/src/App.js
+++ router/src/App.js
@@ -1,53 +1,39 @@
 import React from "react";
 import {
-  BrowserRouter as Router,
   Switch,
   Route,
-  Link,
-  Redirect
+  Redirect,
+    NavLink
 } from "react-router-dom";
+import "./App.css"
+
+
+const urls = [
+  {url: '/' , title: "Home"},
+  {url: "/about" , title: "About"},
+  {url: "/users" , title: "Users"},
+  {url: "/test-route" , title: "Test route"},
+]
+
 
 export default function App() {
   return (
-    <Router>
       <div>
-
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-            <li>
-              <Link to="/test-route">Test route</Link>
-            </li>
-          </ul>
-        </nav>
-
+        <MenuItems items={urls}/>
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
+          <Route exact path="/" component={Home} />
           <Route path="/about">
             <About />
           </Route>
+          < Route path = "/users" component={Users}/>
 
-          < Route path = "/users" component={Users}/>  
-            
-          
-          <Route path="/" exact>
-            {Home}
-          </Route>
+          {/*<Route path = "/test-route" render = {(args) => {// dвикористовуємо якщо на основі цих аргументів хочемо рендерити одне чи інше і прокидувати пропсами*/}
+          {/*console.log(args);*/}
+          {/*return <TestRoute/>*/}
+          {/*}}/>*/}
 
-          <Route path = "/test-route" render = {(args) => {// dвикористовуємо якщо на основі цих аргументів хочемо рендерити одне чи інше і прокидувати пропсами
-          console.log(args);
-          return <TestRoute/>
-          }}/>
-          
           {/* <Route>
             <h1>PAGE NOT FOUND</h1>
           </Route> */}
@@ -56,9 +42,8 @@ export default function App() {
             <Redirect to='/'/>
           </Route>
 
-        </Switch> 
+        </Switch>
       </div>
-    </Router>
   );
 }
 
@@ -75,4 +60,25 @@ function Users() {
 }
 function TestRoute() {
   return <h2>Test route</h2>;
+}
+
+const MenuItems = ({items}) => {
+
+  return(
+      <nav>
+        <ul>
+          {
+            items.map((item) => (
+                <li key={item.title} >
+                  <NavLink exact
+                           to={item.url}>
+                    {item.title}
+                  </NavLink>
+                </li>
+            ))
+          }
+        </ul>
+      </nav>
+
+  )
 }
